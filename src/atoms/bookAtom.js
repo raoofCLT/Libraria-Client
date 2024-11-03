fe7e5e102d1dd4b07bb11d3e5d3e0fa3fe81f269
Abdul Raoof <raoofkottayil@gmail.com>
2024-11-03 20:14:54 +0530

    fix(page) fix error of user page

--- src/atoms/bookAtom.js
+++
@@ -1,8 +0,0 @@
-import {atom} from "recoil"
-
-const bookAtom = atom({
-    key: "bookAtom",
-    default: []
-})
-
-export default bookAtom
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -20,6 +20,8 @@ import {
   ModalBody,
   ModalFooter,
   Tooltip,
+  Text,
+  Divider,
 } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
 import useShowToast from "../hooks/useShowToast";
@@ -29,16 +31,17 @@ import { useNavigate } from "react-router-dom";
 import { ViewIcon, ViewOffIcon, ChevronLeftIcon } from "@chakra-ui/icons";
 
 const UserPage = () => {
-  const showToast = useShowToast();
-  const [currentBooks, setCurrentBooks] = useState([]);
-  const [userUpdate, setUserUpdate] = useState("");
-  const [imageUrl, setImageUrl] = useState(null);
   const [user, setUser] = useRecoilState(userAtom);
   const [showPassword, setShowPassword] = useState(false);
-  const [updating, setUpdating] = useState(false);
-  const fileRef = useRef(null);
-  const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentBooks, setCurrentBooks] = useState([]);
+  const [userUpdate, setUserUpdate] = useState("");
+  const [readBooks, setReadBooks] = useState([]);
+  const [updating, setUpdating] = useState(false);
+  const [imageUrl, setImageUrl] = useState(null);
+  const showToast = useShowToast();
+  const navigate = useNavigate();
+  const fileRef = useRef(null);
   const [inputs, setInputs] = useState({
     name: user.name,
     profilePic:user.profilePic,
@@ -47,6 +50,7 @@ const UserPage = () => {
     password: "",
   });
 
+  //User Details
   useEffect(() => {
     const getUserUpdate = async () => {
       try {
@@ -64,13 +68,14 @@ const UserPage = () => {
     getUserUpdate();
   }, [showToast, user]);
 
+  //Current Books
   useEffect(() => {
     const getCurrentBooks = async () => {
       try {
         if (userUpdate.currentBooks) {
           const fetchedBooks = await Promise.all(
-            userUpdate.currentBooks.map(async (bookId) => {
-              const res = await fetch(`/api/books/getbook/${bookId}`);
+            userUpdate.currentBooks.map(async (book) => {
+              const res = await fetch(`/api/books/getbook/${book.bookId}`);
               return await res.json();
             })
           );
@@ -83,8 +88,35 @@ const UserPage = () => {
     getCurrentBooks();
   }, [showToast, userUpdate]);
 
-  const togglePassword = () => setShowPassword(!showPassword);
+ // Read Books
+  useEffect(() => {
+    const getReadBooks = async () => {
+      try {
+        if (userUpdate.books) {
+ 
+          const bookPromises = userUpdate.books.map(async (bookId) => {
+            const res = await fetch(`/api/books/getbook/${bookId}`);
+            const data = await res.json(); 
+  
+            if (data.error) {
+              showToast("Error", data.error, "error");
+              return null; 
+            }
+            return data;
+          });
 
+        const books = await Promise.all(bookPromises);
+        const filteredBooks = books.filter(book => book !== null);
+          setReadBooks(filteredBooks);
+        }
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      }
+    };
+    getReadBooks();
+  }, [showToast, userUpdate]);
+
+  
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -96,7 +128,7 @@ const UserPage = () => {
       reader.readAsDataURL(file);
     }
   };
-
+  
   const handleUpdate = async (e) => {
     e.preventDefault();
     setUpdating(true);
@@ -107,7 +139,7 @@ const UserPage = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(updatedInputs),
       });
-
+      
       const data = await res.json();
       if (data.error) {
         showToast("Error", data.error, "error");
@@ -123,18 +155,17 @@ const UserPage = () => {
       setIsModalOpen(false);
     }
   };
-
-
+  
   const handleCheckOut = async (bookId) => {
     try {
       const dbUser = await fetch(`/api/users/getuser/${user._id}`);
-      if (!dbUser) {
+      if (!dbUser.ok) {
         showToast("Error", "User not found", "error");
         return;
       }
       const userData = await dbUser.json();
-
-      if (!userData.currentBooks.includes(bookId)) {
+      
+      if (!userData.currentBooks.some(book => book.bookId === bookId)) {
         showToast("Error", "Check in this book first", "error");
         return;
       }
@@ -151,16 +182,20 @@ const UserPage = () => {
       }
   
       showToast("Success", data.message, "success");
-      setCurrentBooks((prevBooks) => prevBooks.filter(book => book._id !== bookId));
-    } catch (error) {
+
+ setCurrentBooks((prevBooks) => prevBooks.filter(book => book.bookId !== bookId));   
+ 
+ } catch (error) {
       showToast("Error", error.message, "error");
     }
   };
 
+  const togglePassword = () => setShowPassword(!showPassword);
+
   const handleBack = () => navigate(-1);
 
   return (
-    <Flex direction={{ base: "column", md: "row" }} mx={4}>
+    <Flex direction="column" mx={4}>
       <IconButton
         aria-label="Go back"
         icon={<ChevronLeftIcon />}
@@ -170,57 +205,77 @@ const UserPage = () => {
         top={75}
         left={4}
       />
-      <Flex
-        width={{ base: "100%", md: "80%" }}
-        p={10}
-        m={5}
-        gap={6}
-        justify="center"
-      >
-        {currentBooks?.map((book) => (
-          <Box
-            align="center"
-            key={book._id}
-            h={{ base: 270, md: 370 }}
-            w={{ base: "170px", md: "200px" }}
-            p={{ base: "", md: "2" }}
-            rounded={10}
-            boxShadow="0px 8px 20px rgba(0, 0, 0, 0.3), 0px 4px 10px rgba(0, 0, 0, 0.2)"
-          >
-            <Box p={2}>
-              <Image
-                src={book.coverPage || "https://via.placeholder.com/150"}
-                alt={book.title || "Book cover"}
-                borderRadius="md"
-                mb={2}
-                objectFit="cover"
-                h={{ base: "170px", md: "250px" }}
-              />
-              <Tooltip label={book.title}>
-                <Heading
-                  size="xs"
-                  mb={2}
-                  color="gray.500"
-                  isTruncated
-                  maxWidth="150px"
-                >
-                  {book.title}
-                </Heading>
-              </Tooltip>
-            </Box>
-            <Button
+<Flex
+    width={"100%"}
+    p={10}
+    m={5}
+    mt={10}
+    gap={6}
+    justify="center"
+    wrap="wrap"
+  >
+    {currentBooks?.length > 0 ? (
+      currentBooks.map((book) => (
+        <Box
+          key={book._id}
+          align="center"
+          h={{ base: 270, md: 370 }}
+          w={{ base: "170px", md: "200px" }}
+          p={{ base: "", md: 2 }}
+          rounded="lg"
+          boxShadow="0px 8px 20px rgba(0, 0, 0, 0.3), 0px 4px 10px rgba(0, 0, 0, 0.2)"
+          transition="transform 0.2s ease-in-out"
+          _hover={{ transform: "scale(1.05)" }}
+          bg="gray.800"
+        >
+          <Box p={2}>
+            <Image
+              src={book.coverPage || "https://via.placeholder.com/150"}
+              alt={book.title || "Book cover"}
+              borderRadius="md"
               mb={2}
-              mx={2}
-              bg="red.500"
-              width={{ base: "85%", md: "92%" }}
-              onClick={()=>{handleCheckOut(book._id)}}
-            >
-              CHECK OUT
-            </Button>
+              objectFit="cover"
+              h={{ base: "170px", md: "250px" }}
+            />
+            <Tooltip label={book.title}>
+              <Heading
+                size="xs"
+                mb={2}
+                color="gray.300"
+                isTruncated
+                maxWidth="150px"
+              >
+                {book.title}
+              </Heading>
+            </Tooltip>
           </Box>
-        ))}
-      </Flex>
-      <Flex ml="auto" mt={5} >
+          <Button
+            mb={2}
+            mx={2}
+            bg="red.500"
+            color="white"
+            width={{ base: "85%", md: "92%" }}
+            _hover={{ bg: "red.600" }}
+            onClick={() => handleCheckOut(book._id)}
+          >
+            CHECK OUT
+          </Button>
+        </Box>
+      ))
+    ) : (
+      <Flex
+      width="100%"
+      height="300px"
+      justify="center"
+      align="center"
+    >
+      <Text fontSize="xl" color="gray.500">
+      Oops, no books here! Check in books to view them in your library.
+      </Text>
+    </Flex>
+    )}
+  </Flex>
+      <Flex position={"absolute"} mt={4} right={0} mr={3}>
         <Button onClick={() => setIsModalOpen(true)}>Update Profile</Button>
       </Flex>
 
@@ -238,7 +293,7 @@ const UserPage = () => {
               <FormControl mb={4}>
                 <Stack direction={["column", "row"]} spacing={6}>
                   <Center mb={1}>
-                    <Avatar size="xl" src={imageUrl || user.profilePic} />
+                    <Avatar size="xl" src={imageUrl || userUpdate.profilePic} />
                   </Center>
                   <Center w="full">
                     <Button w="full" onClick={() => fileRef.current.click()}>
@@ -320,6 +375,62 @@ const UserPage = () => {
           </form>
         </ModalContent>
       </Modal>
+
+<Divider
+      borderColor="cyan.300"
+      borderWidth="1px"
+      my={4}
+      width="70%"
+      mx="auto"
+/>
+
+<Heading mt={3} textAlign={"center"}>Books Read</Heading>
+
+      <Flex
+    width={{ base: "100%", md: "100%" }}
+    p={10}
+    ml={3}
+    gap={6}
+    justify="left"
+    wrap="wrap"
+  >
+    {readBooks?.length > 0 ? (
+      readBooks.map((book) => (
+          <Box p={2} align={"center"} key={book._id}>
+            <Image
+              src={book.coverPage || "https://via.placeholder.com/150"}
+              alt={book.title || "Book cover"}
+              borderRadius="md"
+              mb={2}
+              objectFit="cover"
+              h={{ base: "160px", md: "240px" }}
+            />
+            <Tooltip label={book.title}>
+              <Heading
+                size="xs"
+                mb={2}
+                color="gray.400"
+                isTruncated
+                maxWidth="150px"
+              >
+                {book.title}
+              </Heading>
+            </Tooltip>
+          </Box>
+      ))
+    ) : (
+      <Flex
+      width="100%"
+      height="300px"
+      justify="center"
+      align="center"
+    >
+      <Text fontSize="xl" color="gray.500">
+      It seems you haven&apos;t tracked any books read. Get started today!
+      </Text>
+    </Flex>
+    )}
+  </Flex>
     </Flex>
   );
 };
