512a4ad8a23e9a68eb723f3baf66956dc2d1c59f
Abdul Raoof <raoofkottayil@gmail.com>
2024-11-06 21:03:29 +0530

    refactor(page): improve image upload handling

--- src/pages/UpdateBook.jsx
+++ src/pages/UpdateBook.jsx
@@ -6,6 +6,7 @@ import {
   IconButton,
   Image,
   Input,
+  Skeleton,
   Stack,
 } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
@@ -15,11 +16,9 @@ import { ChevronLeftIcon } from "@chakra-ui/icons";
 
 const UpdateBook = () => {
   const [book, setBook] = useState(null);
-  const showToast = useShowToast();
-  const { id: bookId } = useParams();
   const [updating, setUpdating] = useState(false);
-  const fileRef = useRef(null);
-  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
   const [inputs, setInputs] = useState({
     title: "",
     coverPage: "",
@@ -28,10 +27,15 @@ const UpdateBook = () => {
     publicationDate: "",
     bio: "",
   });
+  const showToast = useShowToast();
+  const { id: bookId } = useParams();
+  const fileRef = useRef(null);
+  const navigate = useNavigate();
 
   //Get Book
   useEffect(() => {
     const getBook = async () => {
+      setLoading(true);
       try {
         const res = await fetch(`/api/books/getbook/${bookId}`);
         const data = await res.json();
@@ -40,6 +44,7 @@ const UpdateBook = () => {
           showToast("Error", data.error, "error");
           return;
         }
+
         setBook(data);
         setInputs({
           title: data.title,
@@ -49,13 +54,32 @@ const UpdateBook = () => {
           publicationDate: data.publicationDate,
           bio: data.bio,
         });
+        setImageUrl(data.coverPage);
       } catch (error) {
         showToast("Error", error.message, "error");
+      } finally {
+        setLoading(false);
       }
     };
     getBook();
   }, [showToast, bookId]);
 
+  // Handle image change
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type.startsWith("image/")) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImageUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      showToast("Invalid file type", "Please select an image file", "error");
+      setImageUrl(null);
+    }
+  };
+
+  // Handle book update
   const handleUpdate = async (e) => {
     e.preventDefault();
     if (!book) {
@@ -63,13 +87,17 @@ const UpdateBook = () => {
       return;
     }
     setUpdating(true);
+
     try {
       const res = await fetch(`/api/books/update/${book._id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(inputs),
+        body: JSON.stringify({
+          ...inputs,
+          coverPage: imageUrl,
+        }),
       });
 
       const data = await res.json();
@@ -77,6 +105,7 @@ const UpdateBook = () => {
         showToast("Error", data.error, "error");
         return;
       }
+
       showToast("Success", "Book updated successfully", "success");
       setBook(data);
       setInputs({
@@ -89,7 +118,6 @@ const UpdateBook = () => {
       });
     } catch (error) {
       showToast("Error", error.message, "error");
-      return;
     } finally {
       setUpdating(false);
     }
@@ -100,174 +128,204 @@ const UpdateBook = () => {
   };
 
   return (
-    <Flex>
-      <IconButton
-        aria-label="Go back"
-        icon={<ChevronLeftIcon />}
-        onClick={handleBack}
-        size="sm"
-        position="absolute"
-        top={75}
-        left={4}
-      />
-      <form style={{ flex: "1" }}>
-        <Stack
-          spacing={5}
-          direction={{ base: "column", md: "row" }}
-          p={2}
-          m={{ base: 5, md: 2 }}
-          align={"center"}
+    <Flex direction="column" align="center" p={5}>
+      {loading ? (
+        <Flex
+          mt={20}
+          h="70%"
+          width="100%"
+          flexDir={{ base: "column", md: "row" }}
+          p={4}
+          gap={8}
+          justify="center"
+          align="center"
         >
-          <Flex direction="column" flex="1" alignItems="center">
-            <Center m={5}>
-              <Image
-                w={{ base: "300px", md: "300px" }}
-                src={inputs.coverPage}
-                rounded={10}
-                boxShadow="0px 8px 20px rgba(0, 0, 0, 0.3), 0px 4px 10px rgba(0, 0, 0, 0.2)"
-              />
-            </Center>
-            <Center>
-              <Button
-                w="150px"
-                onClick={() => fileRef.current.click()}
-                boxShadow="0px 8px 20px rgba(0, 0, 0, 0.3), 0px 4px 10px rgba(0, 0, 0, 0.2)"
-              >
-                Change Cover
-              </Button>
-              <Input
-                type="file"
-                hidden
-                ref={fileRef}
-                // onChange={handleImageChange}
-              />
-            </Center>
+          <Skeleton rounded={10} h="400px" w="250px" />
+          <Flex
+            w={{ base: "100%", md: "50%" }}
+            flexDirection="column"
+            gap={5}
+            align={{ base: "center", md: "flex-start" }}
+          >
+            {[0, 1, 2, 3, 4].map((_, index) => (
+              <Skeleton key={index} rounded={5} h="50px" w="300px" />
+            ))}
           </Flex>
-
-          <Flex flex="1" direction="column" w={"80%"} m={2}>
-            <Stack spacing={4}>
-              <FormControl>
-                <Input
-                  h={"50px"}
-                  type="text"
-                  placeholder={inputs.title}
-                  bg={"gray.200"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{ color: "gray.500" }}
-                  _hover={{ bg: "gray.200" }}
-                  _focus={{
-                    borderColor: "red.400",
-                    boxShadow: "0 0 0 1px red.400",
-                  }}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, title: e.target.value })
-                  }
-                  value={inputs.title}
-                />
-              </FormControl>
-              <FormControl>
-                <Input
-                  h={"50px"}
-                  type="text"
-                  placeholder={inputs.author}
-                  bg={"gray.200"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{ color: "gray.500" }}
-                  _hover={{ bg: "gray.200" }}
-                  _focus={{
-                    borderColor: "red.400",
-                    boxShadow: "0 0 0 1px red.400",
-                  }}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, author: e.target.value })
-                  }
-                  value={inputs.author}
-                />
-              </FormControl>
-              <FormControl>
-                <Input
-                  h={"50px"}
-                  type="text"
-                  placeholder={inputs.genre}
-                  bg={"gray.200"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{ color: "gray.500" }}
-                  _hover={{ bg: "gray.200" }}
-                  _focus={{
-                    borderColor: "red.400",
-                    boxShadow: "0 0 0 1px red.400",
-                  }}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, genre: e.target.value })
-                  }
-                  value={inputs.genre}
-                />
-              </FormControl>
-              <FormControl>
-                <Input
-                  h={"50px"}
-                  type="date"
-                  placeholder={inputs.publicationDate}
-                  bg={"gray.200"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{ color: "gray.500" }}
-                  _hover={{ bg: "gray.200" }}
-                  _focus={{
-                    borderColor: "red.400",
-                    boxShadow: "0 0 0 1px red.400",
-                  }}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, publicationDate: e.target.value })
-                  }
-                  value={inputs.publicationDate}
-                />
-              </FormControl>
-              <FormControl>
-                <Input
-                  h={"50px"}
-                  type="text"
-                  placeholder={inputs.bio}
-                  bg={"gray.200"}
-                  border={0}
-                  color={"gray.500"}
-                  _placeholder={{ color: "gray.500" }}
-                  _hover={{ bg: "gray.200" }}
-                  _focus={{
-                    borderColor: "red.400",
-                    boxShadow: "0 0 0 1px red.400",
-                  }}
-                  onChange={(e) =>
-                    setInputs({ ...inputs, bio: e.target.value })
-                  }
-                  value={inputs.bio}
-                />
-              </FormControl>
-            </Stack>
-            <Button
-              fontFamily={"heading"}
-              boxShadow="0px 8px 20px rgba(0, 0, 0, 0.3), 0px 4px 10px rgba(0, 0, 0, 0.2)"
-              mt={5}
-              h={"50px"}
-              w={"full"}
-              bg="green.500"
-              color="white"
-              _hover={{
-                bg: "green.600",
-                boxShadow: "md",
-              }}
-              type="button"
-              onClick={handleUpdate}
-              isLoading={updating}
+        </Flex>
+      ) : (
+        <>
+          <IconButton
+            aria-label="Go back"
+            icon={<ChevronLeftIcon />}
+            onClick={handleBack}
+            size="sm"
+            position="absolute"
+            top={75}
+            left={4}
+          />
+          <form style={{ flex: "1" }} onSubmit={handleUpdate}>
+            <Stack
+              spacing={5}
+              direction={{ base: "column", md: "row" }}
+              p={2}
+              m={{ base: 5, md: 2 }}
+              align="center"
             >
-              Update
-            </Button>
-          </Flex>
-        </Stack>
-      </form>
+              <Flex direction="column" flex="1" alignItems="center">
+                <Center m={10}>
+                  <Image
+                    w="400px"
+                    src={imageUrl || inputs.coverPage}
+                    rounded={10}
+                    boxShadow="0px 8px 20px rgba(0, 0, 0, 0.3), 0px 4px 10px rgba(0, 0, 0, 0.2)"
+                  />
+                </Center>
+                <Center>
+                  <Button
+                    w="150px"
+                    onClick={() => fileRef.current.click()}
+                    boxShadow="0px 8px 20px rgba(0, 0, 0, 0.3), 0px 4px 10px rgba(0, 0, 0, 0.2)"
+                  >
+                    Change Cover
+                  </Button>
+                  <Input
+                    type="file"
+                    hidden
+                    ref={fileRef}
+                    onChange={handleImageChange}
+                  />
+                </Center>
+              </Flex>
+
+              <Flex flex="1" direction="column"  m={2}>
+                <Stack spacing={4}>
+                  <FormControl>
+                    <Input
+                      h="50px"
+                      type="text"
+                      placeholder="Enter book title"
+                      bg="gray.200"
+                      border={0}
+                      color="gray.500"
+                      _placeholder={{ color: "gray.500" }}
+                      _hover={{ bg: "gray.200" }}
+                      _focus={{
+                        borderColor: "red.400",
+                        boxShadow: "0 0 0 1px red.400",
+                      }}
+                      onChange={(e) =>
+                        setInputs({ ...inputs, title: e.target.value })
+                      }
+                      value={inputs.title}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <Input
+                      h={"50px"}
+                      type="text"
+                      placeholder={inputs.author}
+                      bg={"gray.200"}
+                      border={0}
+                      color={"gray.500"}
+                      _placeholder={{ color: "gray.500" }}
+                      _hover={{ bg: "gray.200" }}
+                      _focus={{
+                        borderColor: "red.400",
+                        boxShadow: "0 0 0 1px red.400",
+                      }}
+                      onChange={(e) =>
+                        setInputs({ ...inputs, author: e.target.value })
+                      }
+                      value={inputs.author}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <Input
+                      h={"50px"}
+                      type="text"
+                      placeholder={inputs.genre}
+                      bg={"gray.200"}
+                      border={0}
+                      color={"gray.500"}
+                      _placeholder={{ color: "gray.500" }}
+                      _hover={{ bg: "gray.200" }}
+                      _focus={{
+                        borderColor: "red.400",
+                        boxShadow: "0 0 0 1px red.400",
+                      }}
+                      onChange={(e) =>
+                        setInputs({ ...inputs, genre: e.target.value })
+                      }
+                      value={inputs.genre}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <Input
+                      h={"50px"}
+                      type="date"
+                      placeholder={inputs.publicationDate}
+                      bg={"gray.200"}
+                      border={0}
+                      color={"gray.500"}
+                      _placeholder={{ color: "gray.500" }}
+                      _hover={{ bg: "gray.200" }}
+                      _focus={{
+                        borderColor: "red.400",
+                        boxShadow: "0 0 0 1px red.400",
+                      }}
+                      onChange={(e) =>
+                        setInputs({
+                          ...inputs,
+                          publicationDate: e.target.value,
+                        })
+                      }
+                      value={inputs.publicationDate}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <Input
+                      h={"50px"}
+                      type="text"
+                      placeholder={inputs.bio}
+                      bg={"gray.200"}
+                      border={0}
+                      color={"gray.500"}
+                      _placeholder={{ color: "gray.500" }}
+                      _hover={{ bg: "gray.200" }}
+                      _focus={{
+                        borderColor: "red.400",
+                        boxShadow: "0 0 0 1px red.400",
+                      }}
+                      onChange={(e) =>
+                        setInputs({ ...inputs, bio: e.target.value })
+                      }
+                      value={inputs.bio}
+                    />
+                  </FormControl>
+                </Stack>
+                <Button
+                  fontFamily={"heading"}
+                  boxShadow="0px 8px 20px rgba(0, 0, 0, 0.3), 0px 4px 10px rgba(0, 0, 0, 0.2)"
+                  mt={5}
+                  h={"50px"}
+                  w={"full"}
+                  bg="green.500"
+                  color="white"
+                  _hover={{
+                    bg: "green.600",
+                    boxShadow: "md",
+                  }}
+                  type="button"
+                  onClick={handleUpdate}
+                  isLoading={updating}
+                >
+                  Update
+                </Button>
+              </Flex>
+            </Stack>
+          </form>
+        </>
+      )}
     </Flex>
   );
 };
