bbcc74fcda4ca3cdde59dbbda6e10c14d3705750
Abdul Raoof <raoofkottayil@gmail.com>
2024-11-07 12:50:31 +0530

    refactor(url):render url apply

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // Get rid of the CORS error
     proxy:{
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://e-library-server-oizs.onrender.com",
         changeOrigin: true,
         secure: false,
       }
