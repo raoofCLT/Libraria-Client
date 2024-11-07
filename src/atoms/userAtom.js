8aa643533de2c4098303fba8154c4d3cbe121985
Abdul Raoof <raoofkottayil@gmail.com>
2024-11-07 15:32:10 +0530

    Update userAtom.js

--- src/atoms/userAtom.js
+++ src/atoms/userAtom.js
@@ -2,7 +2,7 @@ import {atom } from "recoil"
 
 const userAtom = atom ({
     key: "userAtom",
-    default:{})
+    default: JSON.parse(localStorage.getItem("user-library"))
 })
 
 export default userAtom
