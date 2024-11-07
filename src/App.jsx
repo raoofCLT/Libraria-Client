58349c6f60a204a859a6c4317d4d802cfb2bb166
Abdul Raoof <raoofkottayil@gmail.com>
2024-11-07 13:09:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import AuthPage from "./pages/AuthPage";
 import UpdateBook from "./pages/UpdateBook";
 import CreateBook from "./pages/CreateBook";
 import UserAdmin from "./pages/UserAdmin";
-import UserPage from "./pages/userPage";
+import UserPage from "./pages/userPage.jsx";
 
 function App() {
   const user = useRecoilValue(userAtom);
