d22ff8e376ac06f5f433ea87af911c9b67b630c9
Abdul Raoof <raoofkottayil@gmail.com>
2024-11-07 13:39:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     port: 3000,
     // Get rid of the CORS error
-    proxy:{
+    proxy: {
       "/api": {
         target: "https://e-library-server-oizs.onrender.com",
         changeOrigin: true,
         secure: false,
       }
     }
+  },
+  build: {
+    // Increase the chunk size warning limit
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1 MB (default is 500 KB)
+    
+    rollupOptions: {
+      output: {
+        // Manually split chunks, for example, vendor libraries in a separate chunk
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Bundle all dependencies into a single vendor chunk
+          }
+        }
+      }
+    }
   }
-})
+});
+
